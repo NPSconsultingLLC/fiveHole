034f30f67103a9750fd28d0448b3acf63139e802
Nathan Stryker <nathan.stryker@optum.com>
2020-12-16 16:32:33 -0600

    Adding add goalie to stack

--- FiveHole/FiveHole/UI Views/GameInput/Views/Game Input/Add Goalie/GoalieAddSelectPage.swift
+++ FiveHole/FiveHole/UI Views/GameInput/Views/Game Input/Add Goalie/GoalieAddSelectPage.swift
@@ -20,34 +20,26 @@ struct GoalieProfileView: View {
     @State var showingAddNewGoalie = false
     
     var body: some View {
-        VStack{
+        ZStack{
+            VStack{
                 TabView{
-                    ForEach(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { _ in
+                    ForEach(0 ..< 5) { _ in
                         VStack{
                             ZStack{
                                 RoundedRectangle(cornerRadius: 8.0, style: .continuous)
                                     .fill(LinearGradient(.NPSButtonStart, .NPSButtonEnd))
                                     .shadow(color: .NPSDarkStart, radius: 5, x: 3, y: 3)
                                     .shadow(color: .NPSDarkEnd, radius: 5, x: -3, y: -3)
-                                    .onTapGesture(count: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/, perform: {
+                                    .onTapGesture(count: 1, perform: {
                                         self.showingAddNewGoalie.toggle()
                                     })
                                 
                                 Text("Add New Goalie")
                                     .font(.largeTitle)
                                     .foregroundColor(.NPSTextColor)
-                                
-                                if showingAddNewGoalie {
-                                    AddNewGoalieAlert(showingAddNewGoalie: $showingAddNewGoalie)
-                                } else {
-                                    AddNewGoalieAlert(showingAddNewGoalie: $showingAddNewGoalie).hidden()
-                                }
-                            }
-                            
-                            Spacer()
+                            }.padding()
                         }
                     }
-                    .padding()
                 }.frame(width: UIScreen.main.bounds.width, height: 400)
                 .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
                 HStack(alignment: .top){
@@ -77,7 +69,13 @@ struct GoalieProfileView: View {
                 }.frame(width: UIScreen.main.bounds.width, height: 200)
                 .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
             }
+            if showingAddNewGoalie {
+                AddNewGoalieAlert(showingAddNewGoalie: $showingAddNewGoalie)
+            } else {
+                AddNewGoalieAlert(showingAddNewGoalie: $showingAddNewGoalie).hidden()
+            }
         }
+    }
 }
 
 struct GoalieAddSelectPage_Previews: PreviewProvider {
