efe74997b4fc2557774939b39ac64fa28d0bb524
Nathan Stryker <nathan.stryker@optum.com>
2020-12-23 16:59:13 -0600

    adding storekit

--- FiveHole/FiveHole/FiveHoleApp.swift
+++ FiveHole/FiveHole/FiveHoleApp.swift
@@ -9,6 +9,17 @@ import SwiftUI
 
 @main
 struct FiveHoleApp: App {
+    
+    let productIDs = [
+        "com.strykeout.goalieScout2.RemoveAds",
+        "com.strykeout.goalieScout2.Beer",
+        "com.strykeout.goalieScout2.Burger",
+        "com.stryekout.goalieScout2.Coffee",
+        "com.strykeout.goalieScout2.wow",
+    ]
+    
+    @StateObject var storeManager = StoreManager()
+    
     let persistenceController = PersistenceController.shared
     
     init(){
@@ -19,6 +30,9 @@ struct FiveHoleApp: App {
         WindowGroup {
             TabContainerView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .onAppear(perform: {
+                                    storeManager.getProducts(productIDs: productIDs)
+                                })
         }
     }
 }
---
+++ FiveHole/FiveHole/UI Views/GameInput/Views/Settings/In-app purchases/StoreManager.swift
@@ -0,0 +1,42 @@
+//
+//  StoreManager.swift
+//  FiveHole
+//
+//  Created by Stryker, Nathan P on 12/23/20.
+//
+
+import Foundation
+import StoreKit
+
+class StoreManager: NSObject, ObservableObject, SKProductsRequestDelegate {
+    
+    @Published var myProducts = [SKProduct]()
+    var request: SKProductsRequest!
+    
+    func productsRequest(_ request: SKProductsRequest, didReceive response: SKProductsResponse) {
+        print("Did receive response")
+        
+        if !response.products.isEmpty {
+            for fetchedProduct in response.products {
+                DispatchQueue.main.async {
+                    self.myProducts.append(fetchedProduct)
+                }
+            }
+        }
+        
+        for invalidIdentifier in response.invalidProductIdentifiers {
+            print("Invalid identifiers found: \(invalidIdentifier)")
+        }
+    }
+    
+    func getProducts(productIDs: [String]) {
+        print("Start requesting products ...")
+        let request = SKProductsRequest(productIdentifiers: Set(productIDs))
+        request.delegate = self
+        request.start()
+    }
+    
+    func request(_ request: SKRequest, didFailWithError error: Error) {
+        print("Request did fail: \(error)")
+    }
+}
